1d864ebd3554301ba88f127fc5a88cb595fca50e
Trung Kiên <trungkien07112001@gmail.com>
2023-08-09 15:31:23 +0700

    Add angular project Weather App

---
+++ src/app/services/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl:  'https://weatherapi-com.p.rapidapi.com/forecast.json',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '1a0f815557mshdc0fe7d1de78975p1a6393jsn35d81d6ff49a',
+};
